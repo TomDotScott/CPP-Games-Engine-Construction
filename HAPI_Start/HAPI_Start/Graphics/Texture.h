6790d60a2e41ca6496b702d6fe8f668407599bd5
TomDotScott <tom.scott45456@gmail.com>
2020-12-19 14:45:42 +0000

    Texture Header File
    A header file was created for the Texture Struct, to increase the readibility of the TextureManager's interface

---
+++ HAPI_Start/HAPI_Start/Graphics/Texture.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+struct Texture
+{
+	explicit Texture(const std::string& fileName) :
+		textureData(),
+		size(constants::k_spriteSheetCellSize, constants::k_spriteSheetCellSize)
+	{
+		int textureWidth{ 0 };
+		int textureHeight{ 0 };
+		if (!HAPI.LoadTexture(fileName, &textureData, textureWidth, textureHeight))
+		{
+			const std::string message{ "The file: " + fileName + " was not located. Check the file path or the filename." };
+			HAPI.UserMessage(message, "File not found");
+		}
+
+		size = { static_cast<float>(textureWidth), static_cast<float>(textureHeight) };
+	}
+
+	~Texture()
+	{
+		delete[] textureData;
+	}
+
+	// Non-copyable and Non-moveable
+	Texture(const Texture& other) = delete;
+	Texture(Texture&& other) noexcept = delete;
+
+	Texture& operator=(const Texture& other) = delete;
+	Texture& operator=(Texture&& other) noexcept = delete;
+
+	Vector2 GetSize() const
+	{
+		return size;
+	}
+
+	HAPISPACE::BYTE* textureData;
+	Vector2 size;
+};
--- HAPI_Start/HAPI_Start/Graphics/TextureManager.h
+++ HAPI_Start/HAPI_Start/Graphics/TextureManager.h
@@ -4,6 +4,7 @@
 #include "../Design Patterns/Singleton.h"
 #include "../Utilities/Constants.h"
 #include "../Utilities/Vector.h"
+#include "Texture.h"
 
 class TextureManager : public Singleton<TextureManager>
 {
@@ -31,46 +32,7 @@ public:
 	bool CreateSprite(const std::string& spriteName);
 	void DrawSprite(const std::string& name, Vector2 position, bool flipped = false, short alpha = 255);
 
-private:
-	struct Texture
-	{
-		explicit Texture(const std::string& fileName) :
-			textureData(),
-			size(constants::k_spriteSheetCellSize, constants::k_spriteSheetCellSize)
-		{
-			int textureWidth{ 0 };
-			int textureHeight{ 0 };
-			if (!HAPI.LoadTexture(fileName, &textureData, textureWidth, textureHeight))
-			{
-				const std::string message{ "The file: " + fileName + " was not located. Check the file path or the filename." };
-				HAPI.UserMessage(message, "File not found");
-			}
-
-			size = { static_cast<float>(textureWidth), static_cast<float>(textureHeight) };
-		}
-
-		~Texture()
-		{
-			delete[] textureData;
-		}
-
-		// Non-copyable and Non-moveable
-		Texture(const Texture& other) = delete;
-		Texture(Texture&& other) noexcept = delete;
-
-		Texture& operator=(const Texture& other) = delete;
-		Texture& operator=(Texture&& other) noexcept = delete;
-
-		Vector2 GetSize() const
-		{
-			return size;
-		}
-
-		HAPISPACE::BYTE* textureData;
-		Vector2 size;
-	};
-
-	
+private:	
 	HAPISPACE::BYTE* m_screen;
 	std::unordered_map<std::string, Texture*> m_textureBuffer;
 
